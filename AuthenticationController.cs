855b8bab206993f00008d0d4e859794a15a5ef07
RahulSawarkar4444 <69417444+RahulSawarkar4444@users.noreply.github.com>
2020-08-09 16:44:57 +0530

    Add files via upload

---
+++ AuthenticationController.cs
@@ -0,0 +1,110 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web;
+using System.Web.Http;
+
+namespace WebApplication1
+{
+    public class AuthenticationController : ApiController
+    {
+        #region Private Members
+
+        /// <summary>
+        /// Unity container object.
+        /// </summary>
+        private IUnityContainer container;
+
+        /// <summary>
+        /// The authy manager
+        /// </summary>
+        private AuthyManager authyManager;
+
+        #endregion
+
+        #region Constructor
+
+        /// <summary>
+        /// Initializes a new instance of the AuthenticationController class
+        /// </summary>
+        /// <param name="container">the unity container object.</param>
+        public AuthenticationController(IUnityContainer container)
+        {
+            this.container = container;
+            this.authyManager = new AuthyManager();
+        }
+
+        #endregion
+
+        #region Public Methods
+
+        /// <summary>
+        /// This function is responsible for authenticate user.
+        /// </summary>
+        /// <param name="authenticationParameter">The authentication search parameter.</param>
+        /// <returns>Returns user details.</returns>
+        [HttpPost]
+        public JObject Authenticate(Authenticate authenticationParameter)
+        {
+            string ipAddress, deviceType, browserName;
+            try
+            {
+                bool isUserAllowed = false;
+                if (ConfigurationManager.AppSettings["IsActive"].ToString() == "1")
+                {
+                    isUserAllowed = true;
+                }
+                else
+                {
+                    if (authenticationParameter.Email == ConfigurationManager.AppSettings["EmergencyLogin"].ToString())
+                    {
+                        isUserAllowed = true;
+                    }
+                    else
+                    {
+                        throw new Exception(ConfigurationManager.AppSettings["EndMsg"].ToString());
+                    }
+                }
+
+                if (isUserAllowed)
+                {
+                    ipAddress = HttpContext.Current.Request.UserHostAddress;
+                    deviceType = HttpContext.Current.Request.Browser.IsMobileDevice ? DeviceType.Mobile.GetDescription() : DeviceType.Desktop.GetDescription();
+                    browserName = HttpContext.Current.Request.UserAgent;
+                    string tenantCode = Guid.Empty.ToString();
+                    tenantCode = ((IEnumerable<string>)Request.Headers.GetValues(00000000)).FirstOrDefault();
+                    User user = new AuthenticationManager(this.container).UserAuthenticate(authenticationParameter.Email, authenticationParameter.Password, ipAddress, deviceType, browserName, tenantCode, authenticationParameter.IsForcefullyLogout);
+
+                    //Generate Token
+                    string baseAddress = ConfigurationManager.AppSettings["BaseURL"].ToString();
+                    using (var client = new HttpClient())
+                    {
+                        var form = new Dictionary<string, string>
+                       {
+                        {"grant_type", "password"},
+                        {"username", authenticationParameter.Email},
+                        {"password", authenticationParameter.Password},
+                        {"ClientId", tenantCode},
+                        {"User", JsonConvert.SerializeObject(user).ToString()},
+                    };
+                        var tokenResponse = client.PostAsync(baseAddress + "Token", new FormUrlEncodedContent(form)).Result;
+                        var token = tokenResponse.Content.ReadAsStringAsync();
+                        return JsonConvert.DeserializeObject<JObject>(token.Result);
+                    }
+                }
+            }
+            catch
+            {
+                throw;
+            }
+            return null;
+        }
+
+        #endregion
+    }
+}
---
+++ Startup.Auth.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using Microsoft.AspNet.Identity;
+using Microsoft.Owin;
+using Microsoft.Owin.Security.Cookies;
+using Microsoft.Owin.Security.OAuth;
+using Owin;
+//using OAuthWebApiApp.Providers;
+using Microsoft.Owin.Security;
+
+using System.Web.Http;
+
+namespace WebApplication1.App_Start
+{
+    public partial class Startup
+    {
+        public static OAuthAuthorizationServerOptions OAuthOptions { get; private set; }
+
+        public static string PublicClientId { get; private set; }
+
+        // For more information on configuring authentication, please visit http://go.microsoft.com/fwlink/?LinkId=301864
+        public void ConfigureAuth(IAppBuilder app)
+        {
+
+
+            app.UseCookieAuthentication(new CookieAuthenticationOptions
+            {
+                AuthenticationType = "ExternalCookie",
+                AuthenticationMode = AuthenticationMode.Passive,
+                CookieName = "aspouthcookie",
+                ExpireTimeSpan = TimeSpan.FromMinutes(5)
+            });
+
+
+            app.UseExternalSignInCookie(DefaultAuthenticationTypes.ExternalCookie);
+
+            // Configure the application for OAuth based flow
+            PublicClientId = "self";
+            OAuthOptions = new OAuthAuthorizationServerOptions
+            {
+                TokenEndpointPath = new PathString("/Token"),
+                Provider = new ApplicationOAuthProvider(PublicClientId),
+                RefreshTokenProvider = new RefreshTokenProvider(),
+                //AuthorizeEndpointPath = new PathString("/api/Account/ExternalLogin"),
+                AuthorizeEndpointPath = new PathString("/User/ExternalLogin"),
+
+                AccessTokenExpireTimeSpan = TimeSpan.FromMinutes(50),
+                // In production mode set AllowInsecureHttp = false
+                AllowInsecureHttp = true
+            };
+
+        }
+    }
+}
